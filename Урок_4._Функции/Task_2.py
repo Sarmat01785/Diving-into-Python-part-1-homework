7600d00cb9fc21e59bf7ca5cddfa55e641dd63d1
Sarmat <Spirator@Yandex.ru>
2024-08-17 18:07:38 +0700

    Задача 2 решена

--- Урок_4._Функции/Task_2.py
+++ Урок_4._Функции/Task_2.py
@@ -5,3 +5,15 @@
 и возвращающую словарь, где ключ — значение переданного аргумента, а значение — имя аргумента. 
 Если ключ не хешируем, используйте его строковое представление.
 '''
+def args_to_dict(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        try:
+            hash(value)
+        except TypeError:
+            value = str(value)
+        result[value] = key
+    return result
+
+result_dict = args_to_dict(name="Alice", age=25, salary="10000")
+print(result_dict)
