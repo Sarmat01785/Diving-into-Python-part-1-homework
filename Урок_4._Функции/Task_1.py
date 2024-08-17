f7ca39ba518a823e78c3fb77be5d5e7e7330727f
Sarmat <Spirator@Yandex.ru>
2024-08-17 17:32:01 +0700

    Formatting the document

--- Урок_4._Функции/Task_1.py
+++ Урок_4._Функции/Task_1.py
@@ -1,17 +1,16 @@
-'''
+"""
 Задача 1
 
 Напишите функцию для транспонирования матрицы
-'''
+"""
+
+
 def transpose(matrix):
-    
+
     return [list(row) for row in zip(*matrix)]
 
-original_matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+
+original_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 transposed_matrix = transpose(original_matrix)
 for row in transposed_matrix:
