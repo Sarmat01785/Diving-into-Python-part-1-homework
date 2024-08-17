54dd3e21efbc7ca1aa1d1c11adf14795f6c27c18
Sarmat <Spirator@Yandex.ru>
2024-08-17 17:29:35 +0700

    Задача 1 решена

--- Урок_4._Функции/Task_1.py
+++ Урок_4._Функции/Task_1.py
@@ -3,3 +3,16 @@
 
 Напишите функцию для транспонирования матрицы
 '''
+def transpose(matrix):
+    
+    return [list(row) for row in zip(*matrix)]
+
+original_matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+transposed_matrix = transpose(original_matrix)
+for row in transposed_matrix:
+    print(row)
