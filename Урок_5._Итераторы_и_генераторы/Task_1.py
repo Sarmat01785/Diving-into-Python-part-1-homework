073d28636f2fb8b57a880188f1470f724c4c4433
Sarmat <Spirator@Yandex.ru>
2024-08-19 14:00:03 +0700

    end

--- Урок_5._Итераторы_и_генераторы/Task_1.py
+++ Урок_5._Итераторы_и_генераторы/Task_1.py
@@ -1,6 +1,20 @@
-'''
+"""
 Задача 1
 
 Напишите функцию, которая принимает на вход строку - абсолютный путь до файла. 
 Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
-'''
+"""
+
+import os
+
+
+def get_file_info(path):
+    dir_path, filename = os.path.split(path)
+
+    name, ext = os.path.splitext(filename)
+
+    return dir_path, name, ext
+
+
+print("\n")
+print(get_file_info(r"C:\Users\Сармат\Documents\Обучение\Новый текстовый документ.txt"))
