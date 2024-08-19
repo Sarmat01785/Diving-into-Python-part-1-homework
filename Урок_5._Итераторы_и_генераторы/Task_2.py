cff55fb910334de0d4d0e40350d49410451a062b
Sarmat <Spirator@Yandex.ru>
2024-08-19 14:00:27 +0700

    end

--- Урок_5._Итераторы_и_генераторы/Task_2.py
+++ Урок_5._Итераторы_и_генераторы/Task_2.py
@@ -6,3 +6,17 @@
 В результате получаем словарь с именем в качестве ключа и суммой премии в качестве значения. 
 Сумма рассчитывается как ставка умноженная на процент премии.
 """
+
+
+def create_premium_dictionary(names, rates, premiums):
+    return {
+        name: rate * (float(premium.strip("%")) / 100)
+        for name, rate, premium in zip(names, rates, premiums)
+    }
+
+
+names = ["Анна", "Борис", "Виктор"]
+rates = [50000, 60000, 70000]
+premiums = ["10.25%", "15.5%", "12%"]
+
+print(create_premium_dictionary(names, rates, premiums))
