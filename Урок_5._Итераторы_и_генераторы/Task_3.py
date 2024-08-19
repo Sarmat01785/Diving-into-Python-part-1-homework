ebf953f733f55c5f0340a777ef16716697b192b2
Sarmat <Spirator@Yandex.ru>
2024-08-19 14:00:46 +0700

    end

--- Урок_5._Итераторы_и_генераторы/Task_3.py
+++ Урок_5._Итераторы_и_генераторы/Task_3.py
@@ -3,3 +3,17 @@
 
 Создайте функцию генератор чисел Фибоначчи
 """
+
+
+def fibonacci_generator(limit=None):
+    a, b = 0, 1
+    count = 0
+
+    while limit is None or count < limit:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+
+fib_gen = fibonacci_generator(limit=10)
+print(list(fib_gen))
